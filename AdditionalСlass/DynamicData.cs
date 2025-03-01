62a25edbd6294ad3b331370596adb07ee7339b71
CyberPathfinder <a.v.ghostwolf@gmail.com>
2025-03-01 10:51:09 +0300

    1.1

---
+++ AdditionalСlass/DynamicData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Keyboard_simulator.AdditionalСlass
+{
+    struct DynamicData
+    {
+        public static String Systemlanguage;
+        public static Int32 NumberOfCharacters = 0;
+        public static String[] Text = new string[1];
+        public static int LessonNumber;
+        public static Boolean ShowPicture = true;
+        public static Boolean Stage3 = false;
+        public static string Time;
+        public static bool Sound = true;
+        public static WindowState windowState;
+    }
+}
